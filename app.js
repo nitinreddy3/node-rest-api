292ae1bd81dbbe5e5b668b79ea015b155121973f
Nitin Reddy <reddynitin3@gmail.com>
2017-09-19 07:26:51 +0530

    Initialized server

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+mongoose.connect('mongodb://localhost/bookstore');
+
+var db = mongoose.connection;
+
+app.get('/', (req, res) => {
+    res.send('Hello Puja Rani')
+})
+
+app.listen("8000");
+console.log("listens to port 8000...")
